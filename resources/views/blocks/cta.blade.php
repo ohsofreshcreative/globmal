f10560b3bb46109de0d1df1b83bd3013c0855c44
Andżelika <andzelika@MacBook-Pro.local>
2026-07-29 11:51:15 +0200

    Automatyczny deploy

--- resources/views/blocks/cta.blade.php
+++ resources/views/blocks/cta.blade.php
@@ -8,8 +8,8 @@
 	$section_class => filled($section_class),
 	$background => filled($background) && $background !== 'none',
 	])>
-	<div class="absolute inset-0 pointer-events-none" style="background: linear-gradient(181deg, #0065A7 -105.34%, #002238 98.92%) "></div>
-	<div class="__wrapper c-main relative overflow-hidden c-main radius py-8 md:py-14 mx-4">
+	<div class="absolute inset-0 pointer-events-none" style="background: linear-gradient(181deg, #0065A7 -105%, #002238 98%) "></div>
+	<div class="__wrapper c-main relative overflow-hidden c-main radius py-8 mx-4">
 		<div class="absolute inset-0 z-10 radius" style="background: linear-gradient(90deg,rgba(0, 51, 84, 0.60) 0.01%, rgba(0, 101, 167, 0.60) 99.99%);"></div>
 		@if (!empty($g_octa['image']))
 		<x-picture
@@ -17,7 +17,7 @@
 			figure-class="absolute inset-0 m-0 z-0"
 			class="w-full h-full object-cover" />
 		@endif
-		<div class="absolute inset-0 z-1 pointer-events-none" style="background: linear-gradient(90deg, rgba(0, 101, 167, 0.50) 0%, rgba(0, 101, 167, 0.50) 100%);"></div>
+	<div class="absolute inset-0 z-10 bg-[#0065A7]/70 pointer-events-none"></div>
 		<div class="__inside  grid grid-cols-1 md:grid-cols-2 items-center gap-6 relative z-20">
 			<div class="__content w-full ">
 				@if (!empty($g_octa['header']))
